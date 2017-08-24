a69124a595d27305d5d4bbde133cd1f48ea9238d
Raluca Plugariu <raluca.plugariu@cgm.com>
2017-08-24 15:52:09 +0300

    another modification in main

--- com/cgm/main/Test.java
+++ com/cgm/main/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is the master");
 		System.out.println("This is the newBranch");
+		Student s1 = new Student("Popescu Adrian","Informatica",20);
+		System.out.println(s1);
+
 
 	}
 
